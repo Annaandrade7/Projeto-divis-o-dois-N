714e9966df6a3b3efb55f82f23c8fc3b7c29f884
Anna Andrade <annaandrade2307@gmail.com>
2023-05-30 15:29:06 -0400

    primeira tenativa de execução

--- ConecçãoBanco/Program.cs
+++ ConecçãoBanco/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+try
+{
+    Console.WriteLine("Informe o primeiro valor!");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe o segundo valor!");
+    int b = int.Parse(Console.ReadLine());
+    int result = a / b;
+    Console.WriteLine(result);
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Algo de errado não esta certo!");
+}
