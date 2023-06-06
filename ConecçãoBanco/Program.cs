babf556e989fcdfcd50513d5475de176bb98015c
Anna Andrade <annaandrade2307@gmail.com>
2023-06-06 14:13:52 -0400

    Atualização

--- ConecçãoBanco/Program.cs
+++ ConecçãoBanco/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-try
+try //tratamento de exeção 
 {
     Console.WriteLine("Informe o primeiro valor!");
     int a = int.Parse(Console.ReadLine());
@@ -8,7 +8,19 @@ try
     int result = a / b;
     Console.WriteLine(result);
 }
+catch(DivideByZeroException ex)
+{
+    Console.WriteLine("Algo de errado não esta certo!"+ ex.Message);
+}
+catch(FormatException ex)
+{
+    Console.WriteLine("Conversão de string para letra não é permitida!"+ ex.Message);
+}
 catch(Exception ex)
 {
-    Console.WriteLine("Algo de errado não esta certo!");
+    Console.WriteLine("Ocorreu um erro!"+ ex.Message);
+}
+finally
+{
+    Console.WriteLine("Passa aa");
 }
